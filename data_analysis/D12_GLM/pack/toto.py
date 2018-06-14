0d44c69ea79106f4e45fb02106257c2d6b5c6727
vincentvigon <vincent.vigon@gmail.com>
2018-06-14 09:57:14 +0200

    vect alea

---
+++ data_analysis/D12_GLM/pack/toto.py
@@ -0,0 +1,12 @@
+
+
+import numpy as np
+
+
+def readX():
+
+    x=np.loadtxt("data/data0_x.csv")
+
+    print(x)
+
+
---
+++ data_analysis/D12_GLM/toto2.py
@@ -0,0 +1,14 @@
+from data_analysis.D12_GLM.pack.toto import readX
+import scipy.stats as st
+
+import numpy as np
+
+np.random.normal()
+
+
+readX()
+
+
+
+
+
